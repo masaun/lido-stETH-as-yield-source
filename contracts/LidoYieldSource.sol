df2f524f1b488b068f31d96a46a1da1ab1c0ef3b
masaun <masanoriuno@gmail.com>
2021-05-17 10:08:34 +0900

    [SC]: Add logic to balanceOfToken() in the LidoYieldSource.sol

--- contracts/LidoYieldSource.sol
+++ contracts/LidoYieldSource.sol
@@ -3,6 +3,7 @@ pragma solidity 0.6.12;
 
 import { IYieldSource } from "./pooltogether/IYieldSource.sol";
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { StETHMockERC20 } from "./lido/mocks/StETHMockERC20.sol";
 import { WstETH } from "./lido/WstETH.sol";
 
 
@@ -10,23 +11,40 @@ import { WstETH } from "./lido/WstETH.sol";
 contract LidoYieldSource is IYieldSource {
     using SafeMath for uint256;
 
+    StETHMockERC20 public stETH;
     WstETH public wstETH;
     address WST_ETH;
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) public balances;  /// Underlying asset balance
 
-    constructor(WstETH _wstETH) public {
+    constructor(StETHMockERC20 _stETH, WstETH _wstETH) public {
+        stETH = _stETH;
         wstETH = _wstETH;
         WST_ETH = address(wstETH);
     }
 
     /// @notice Returns the ERC20 asset token used for deposits.
     /// @return The ERC20 asset token
-    function depositToken() public view override returns (address) {}
+    function depositToken() public view override returns (address) {
+        return WST_ETH;        
+    }
 
-    function balanceOfToken(address addr) public override returns (uint256) {}
+    /// @notice Returns the total balance (in asset tokens).  This includes the deposits and interest.
+    /// @return The underlying balance of asset tokens
+    function balanceOfToken(address addr) public override returns (uint256) {
+        if (balances[addr] == 0) return 0;
 
+        uint256 stETHBalance = stETH.balanceOf(addr);
+        return stETHBalance;
+    }
+
+    /// @notice Allows assets to be supplied on other user's behalf using the `to` param.
+    /// @param amount The amount of `token()` to be supplied
+    /// @param to The user whose balance will receive the tokens
     function supplyTokenTo(uint256 amount, address to) public override {}
 
+    /// @notice Redeems tokens from the yield source from the msg.sender, it burn yield bearing tokens and return token to the sender.
+    /// @param amount The amount of `token()` to withdraw.  Denominated in `token()` as above.
+    /// @return The actual amount of tokens that were redeemed.
     function redeemToken(uint256 amount) public override returns (uint256) {}
 
 }
